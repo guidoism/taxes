b730a287dcc661a752a45e9c5cbfa5c32aac5076
Guido Bartolucci <guido@bartolucci.org>
2017-02-11 21:24:22 -0800

    Added silver

--- update_spot_price_history.py
+++ update_spot_price_history.py
@@ -1,61 +1,64 @@
+# Yes, I know I'm using regexes to parse html, quick breaking my
+# balls, these are simple pages and if the parsing breaks I don't
+# care.
+
 import re
 import json
 import requests
 
-r = requests.get('http://onlygold.com/Info/Search-Gold-Prices.asp')
+def update_gold():
+    r = requests.get('http://onlygold.com/Info/Search-Gold-Prices.asp')
 
-SEED = [
-    ("2016-08-10", "1347.70"),
-    ("2016-08-09", "1341.00"),
-    ("2016-08-08", "1336.80"),
-    ("2016-08-05", "1340.40"),
-    ("2016-08-04", "1362.75"),
-    ("2016-08-03", "1358.90"),
-    ("2016-08-02", "1363.75"),
-    ("2016-08-01", "1349.65"),
-    ("2016-07-29", "1342.00"),
-    ("2016-07-28", "1341.75"),
-    ("2016-07-27", "1329.00"),
-    ("2016-07-26", "1323.00"),
-    ("2016-07-25", "1313.15"),
-    ("2016-07-22", "1320.75"),
-    ("2016-07-21", "1321.15"),
-]
-SEED.sort(reverse=True)
-#print json.dumps(SEED)
+    MONTHS = {
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12',
+    }
 
-MONTHS = {
-    'Jan': '01',
-    'Feb': '02',
-    'Mar': '03',
-    'Apr': '04',
-    'May': '05',
-    'Jun': '06',
-    'Jul': '07',
-    'Aug': '08',
-    'Sep': '09',
-    'Oct': '10',
-    'Nov': '11',
-    'Dec': '12',
-}
-
-prices = dict(json.load(open('gold.json')))
-date = None
-price = None
-for s in r.text.split('\n'):
-    if 'priceGriddata' in s:
-        m = re.search(r'\s+([A-Z][a-z][a-z]) (\d\d), (\d\d\d\d)', s)
-        if m:
-            date = '-'.join((m.group(3), MONTHS[m.group(1)], m.group(2)))
-        else:
-            m = re.search(r'\$(\d?,?\d\d\d\.\d\d)', s)
+    prices = dict(json.load(open('gold.json')))
+    date = None
+    price = None
+    for s in r.text.split('\n'):
+        if 'priceGriddata' in s:
+            m = re.search(r'\s+([A-Z][a-z][a-z]) (\d\d), (\d\d\d\d)', s)
             if m:
-                price = m.group(1).replace(',', '')
-        if date and price:
-            prices[date] = price
-            date = None
-            price = None
+                date = '-'.join((m.group(3), MONTHS[m.group(1)], m.group(2)))
+            else:
+                m = re.search(r'\$(\d?,?\d\d\d\.\d\d)', s)
+                if m:
+                    price = m.group(1).replace(',', '')
+            if date and price:
+                prices[date] = price
+                date = None
+                price = None
+    
+    out = sorted(prices.items(), reverse=True)
+    json.dump(out, open('gold.json', 'w'), indent=2)
+    open('gold.csv', 'w').write('\n'.join((','.join(line) for line in out)))
 
-out = sorted(prices.items(), reverse=True)
-json.dump(out, open('gold.json', 'w'), indent=2)
-open('gold.csv', 'w').write('\n'.join((','.join(line) for line in out)))
+def update_silver():
+    html = requests.get('http://www.pmbull.com/silver-price/').text
+    r = re.compile(r'<table id="historical-silver(.+?)</table>', re.DOTALL)
+    m = r.search(html)
+    table = m.group(1).replace('$', '')
+    r = re.compile(r'<td>(.*?)</td>', re.DOTALL)
+    t = r.findall(table)
+
+    prices = dict(json.load(open('silver.json')))
+    prices.update(dict(zip(t[::4], t[1::4])))
+    out = sorted(prices.items(), reverse=True)
+    json.dump(out, open('silver.json', 'w'), indent=2)
+    open('silver.csv', 'w').write('\n'.join((','.join(line) for line in out)))
+
+    
+#update_gold()
+update_silver()
